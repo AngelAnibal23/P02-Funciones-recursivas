7733ac795b9fc7103af3828291c257562dfcd16e
Guts <aamamanica@unjbg.edu.pe>
2024-09-19 20:51:12 -0500

    d2w1q

--- recursion4.cpp
+++ recursion4.cpp
@@ -1,29 +1,24 @@
-/*
-Escribir una funcion recursiva que dado un numero entero positivo (no string) calcule su imagen especular. Por ejemplo f(345) = 543
-*/
 #include <iostream> 
 using namespace std; 
 
-int invertirNumero(int, int); 
+int invertirNumero(int a, int aux){
+	if(a==0){
+		return aux; 
+	}
+	else{
+		aux = (10 * aux) + (a%10); 
+		
+		return invertirNumero(a/10, aux); 
+	}
+}
 
 int main(){
-	int numero, invertido = 0; 
-	
+	int numero, aux = 0;  
 	cout<<"Escriba un numero: "; 
 	cin>>numero; 
 	
-	cout<<"\nLa imagen especular del numero ingresado es: "<<"f("<<numero<<") = "<<invertirNumero(numero, invertido); 
+	cout<<"\nEl resultado es: "<<"f("<<numero<<") = "<<invertirNumero(numero, aux); 
 	
 	return 0; 
 }
 
-int invertirNumero(int a, int invertir){
-	if(a==0){
-		return invertir; 
-	}
-	else{
-		invertir = (10 * invertir) + (a%10); 
-		
-		return invertirNumero(a/10, invertir); 
-	}
-}
