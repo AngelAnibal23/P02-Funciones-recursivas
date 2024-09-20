12e626ea3a6d5e904d08006088b448be54fa12ea
Guts <aamamanica@unjbg.edu.pe>
2024-09-19 20:50:23 -0500

    3

--- recursion1.cpp
+++ recursion1.cpp
@@ -1,34 +1,29 @@
-/*
-Escriba un metodo recursivo que calcule la potencia de n de un numero x (n es un numero natural)
-*/
-
 #include <iostream> 
 using namespace std; 
 
-int potencia(int, int); 
+int potencia(int a, int b){
+	if(b==0){
+		return 1; 
+	}
+	else if(b==1){
+		return a; 
+	}
+	else{
+		return a * potencia(a,b-1); 
+	}
+}
 
 int main(){
-	int a, b; 
+	int n, x; 
 	
 	cout<<"Digite un numero: "; 
-	cin>>a; 
+	cin>>n; 
 	cout<<"Digite la potencia a la que elevara el numero: "; 
-	cin>>b; 
+	cin>>x; 
 	
-	cout<<"\nEl resultado de "<<a<<" elevado a "<<b<<" es: "<<potencia(a,b); 
+	cout<<"\nEl resultado de "<<n<<" elevado a "<<x<<" es: "<<potencia(n,x); 
 	
 	
 	return 0;
 }
 
-int potencia(int x, int y){
-	if(y==0){
-		return 1; 
-	}
-	else if(y==1){
-		return x; 
-	}
-	else{
-		return x * potencia(x,y-1); 
-	}
-}
